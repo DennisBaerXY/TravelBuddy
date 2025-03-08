3e083e03cad51b8cf2c75d57a35cddebc201f297
Dennis Bär <Dennis-baer@gmx.net>
2025-03-08 14:12:42 +0100

    Initial Commit

---
+++ TravelBuddy/TravelBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelBuddyApp.swift
+//  TravelBuddy
+//
+//  Created by Dennis Bär on 08.03.25.
+//
+
+import SwiftUI
+
+@main
+struct TravelBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
